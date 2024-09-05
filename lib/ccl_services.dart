2573a4216a6fc9e7d123a3257fd261135ca26223
Pasan Eramusugoda <vaprera91@gmail.com>
2024-09-05 18:08:51 +0530

    refactor(Code): Package updates and code restructure

--- lib/ccl_services.dart
+++ lib/ccl_services.dart
@@ -1,9 +1,3 @@
 library ccl_services;
 
-export 'package:ccl_services/notification/notification_service.dart'
-    show NotificationService;
-export 'package:ccl_services/secure_storage/secure_storage_service.dart'
-    show SecureStorageService, SSObject;
-export 'package:ccl_services/localization/localization_service.dart'
-    show LocalizationService
-    hide ILocalizationService;
+export 'src/services.dart';
