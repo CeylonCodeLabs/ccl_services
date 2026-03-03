27dde4f4e563c987f8bfb24b77f4d6000d259ce0
Pasan Eramusugoda <vaprera91@gmail.com>
2026-03-03 15:56:23 +0530

    build(SecureStorage): update dependencies and storage config
    This commit updates `flutter_secure_storage` to v10.0.0, along with minor updates to `shared_preferences` and `uuid`.
    
    Additionally, it modifies the `FlutterSecureStorage` configuration by removing the `encryptedSharedPreferences: true` option from `AndroidOptions`, which is no longer recommended or required in the latest version of the library.

--- lib/src/secure_storage/helpers/secure_storage_instance.dart
+++ lib/src/secure_storage/helpers/secure_storage_instance.dart
@@ -6,7 +6,6 @@ abstract class SecureStorageInstance {
   @protected
   final FlutterSecureStorage defaultInstance = const FlutterSecureStorage(
     aOptions: AndroidOptions(
-      encryptedSharedPreferences: true,
       keyCipherAlgorithm:
           KeyCipherAlgorithm.RSA_ECB_OAEPwithSHA_256andMGF1Padding,
       storageCipherAlgorithm: StorageCipherAlgorithm.AES_GCM_NoPadding,
